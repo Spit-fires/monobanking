36a8b67299c3466bac112c9a80a58b5271d85901
akashfahad12@gmail.com <akashfahad12@gmail.com>
2022-09-18 13:29:21 +0600

    v1

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import math
+from rich.pretty import pprint
 from rich.console import Console
 from rich.table import Table
-from rich.print import Print as rprint
-
+from rich import Print as rprint
+from rich.prompt import Prompt as prompt
+import os
 
 
 console = Console()
 loaners = {}
 bankbal = 0
-
-def makegui():
-  loanlist = Table(title="Loaner list")
+width = os.get_terminal_size()
+linebreaker = """      
+  ___ 
+ |___|
+      
+      """
+loanlist = Table(title="Loaner list")
   
-  loanlist.add_column("Name", style="cyan", no_wrap=True)
-  loanlist.add_column("Amount", style="magenta")
+loanlist.add_column("Name", style="cyan", no_wrap=True)
+loanlist.add_column("Amount", style="magenta")
+      
+      
+def makegui():
+  print("""\  __  __              ___            _   _           
+ |  \/  |___ _ _  ___| _ ) __ _ _ _ | |_(_)_ _  __ _ 
+ | |\/| / _ \ ' \/ _ \ _ \/ _` | ' \| / / | ' \/ _` |
+ |_|  |_\___/_||_\___/___/\__,_|_||_|_\_\_|_||_\__, |
+                                               |___/ """)
+  print(linebreaker.ljust(width, linebreaker))
+  print(loanlist)
+  print(linebreaker.ljust(width, linebreaker))
+  print("Balance: " + bankbal)
+  print(linebreaker.ljust(width, linebreaker))
+  print("Actions:" + \n + "1 = Add a loaner" + \n "2 = Remove a loaner")
+  choice = prompt.ask("What do you want to do?", choices=["1", "2"])
+  
   
 def updategui():
   for name in loaners.keys():
@@ -42,15 +64,37 @@ def removeloaner(name):
     success = "Success"
     return success
   
+def clear():
+ 
 
+    # for windows
+
+    if os.name == 'nt':
+
+        _ = os.system('cls')
+ 
+
+    # for mac and linux(here, os.name is 'posix')
+
+    else:
+
+        _ = os.system('clear')
 
 def main():
-
-
-
-
-
-
+  makegui()
+  if choice == "1":
+    name = input("Name:")
+    amount = input("Amount:")
+    addloaner(name, amount)
+    updategui()
+    clear()
+    makegui()
+  if choice == "2":
+    name = input("Name:")
+    removeloaner(name)
+    updategui()
+    clear()
+    makegui()
   
 while True: 
   main()
